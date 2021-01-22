f7f5a5af72adcd09dcca406605542b16f3fcf128
Alain Lardinois <43622266+ParrotLync@users.noreply.github.com>
2021-01-23 00:28:43 +0100

    Add supreme holo's

--- src/main/java/nl/parrotlync/discovoutlines/manager/ReferenceManager.java
+++ src/main/java/nl/parrotlync/discovoutlines/manager/ReferenceManager.java
@@ -2,9 +2,14 @@ package nl.parrotlync.discovoutlines.manager;
 
 import com.gmail.filoghost.holographicdisplays.api.Hologram;
 import com.gmail.filoghost.holographicdisplays.api.HologramsAPI;
+import com.gmail.filoghost.holographicdisplays.api.handler.TouchHandler;
+import com.gmail.filoghost.holographicdisplays.api.line.TextLine;
 import nl.parrotlync.discovoutlines.DiscovOutlines;
 import nl.parrotlync.discovoutlines.model.ReferencePoint;
+import nl.parrotlync.discovoutlines.util.ChatUtil;
 import nl.parrotlync.discovoutlines.util.DataUtil;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
@@ -32,7 +37,7 @@ public class ReferenceManager {
     }
 
     public ReferencePoint getPoint(String name) {
-        return referencePoints.get(name);
+        return referencePoints.get(name.toLowerCase());
     }
 
     public List<ReferencePoint> getPoints() {
@@ -51,19 +56,24 @@ public class ReferenceManager {
     }
 
     public void save() {
+        for (ReferencePoint point : referencePoints.values()) {
+            removeHologram(point);
+        }
         DataUtil.saveObjectToPath(referencePoints, "plugins/DiscovOutlines/points.data");
         DiscovOutlines.getInstance().getLogger().info("Saved " + referencePoints.size() + " to file.");
     }
 
     private void createHologram(ReferencePoint point) {
-        Hologram hologram = HologramsAPI.createHologram(DiscovOutlines.getInstance(), point.getLocation().add(0, 2, 0));
-        hologram.appendTextLine("§c§lReference Point");
-        hologram.appendTextLine("§7" + point.getName());
+        final Hologram hologram = HologramsAPI.createHologram(DiscovOutlines.getInstance(), point.getLocation().add(0, 2, 0));
+        hologram.appendTextLine("§c§lReference Point").setTouchHandler(getTouchHandler(hologram));
+        hologram.appendTextLine("§7" + point.getName()).setTouchHandler(getTouchHandler(hologram));
         hologram.getVisibilityManager().setVisibleByDefault(false);
+        hologram.getVisibilityManager().resetVisibilityAll();
         holograms.put(point.getName().toLowerCase(), hologram);
     }
 
     private void removeHologram(ReferencePoint point) {
+        holograms.get(point.getName().toLowerCase()).delete();
         holograms.remove(point.getName().toLowerCase());
     }
 
@@ -77,5 +87,14 @@ public class ReferenceManager {
         for (Hologram hologram : holograms.values()) {
             hologram.getVisibilityManager().hideTo(player);
         }
+    }
+
+    private TouchHandler getTouchHandler(Hologram hologram) {
+        return player -> {
+            String name = ChatColor.stripColor(((TextLine) hologram.getLine(1)).getText());
+            ReferencePoint point = getPoint(name);
+            DiscovOutlines.getInstance().getSelectionManager().select(player, point);
+            player.sendMessage("§7Selected the point §6" + point.getName());
+        };
     }
 }
