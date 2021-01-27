bc4816748da4d7ecf62973d77c582998ebaadbab
Alain Lardinois <43622266+ParrotLync@users.noreply.github.com>
2021-01-27 01:36:32 +0100

    Remove redundant import

--- src/main/java/nl/parrotlync/discovoutlines/model/ReferencePoint.java
+++ src/main/java/nl/parrotlync/discovoutlines/model/ReferencePoint.java
@@ -1,8 +1,5 @@
 package nl.parrotlync.discovoutlines.model;
 
-import com.gmail.filoghost.holographicdisplays.api.Hologram;
-import com.gmail.filoghost.holographicdisplays.api.HologramsAPI;
-import nl.parrotlync.discovoutlines.DiscovOutlines;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.World;
